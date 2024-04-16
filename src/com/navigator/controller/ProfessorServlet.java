b3511fafe4eda2549cd6b554dd66a98f59fedd6b
Ziyu-Jerry-Huang <ziyu_h@126.com>
2024-04-16 16:51:49 -0700

    update Professor Servlet

---
+++ src/com/navigator/controller/ProfessorServlet.java
@@ -0,0 +1,15 @@
+package com.navigator.controller;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/update/professor")
+public class ProfessorServlet extends HttpServlet {
+
+  @Override
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
+
+  }
+}
