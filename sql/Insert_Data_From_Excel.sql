c4260902b2a4b1a2b6e412c7aab8bb6e13162403
YillonMask <yixinrui24@gmail.com>
2024-04-22 21:00:37 -0700

    combine sql files into one

--- sql/Insert_Data_From_Excel.sql
+++ sql/Insert_Data_From_Excel.sql
@@ -5,3 +5,17 @@ FIELDS TERMINATED BY ',' ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS
 (name, campus_id);
+
+LOAD DATA LOCAL INFILE '/Users/xinruiyi/Documents/GitHub/rate-my-prof/csv/Comments_final_matched.csv'
+INTO TABLE reviews
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(course_id,professor_id,rating,comment);
+
+LOAD DATA LOCAL INFILE '/Users/xinruiyi/Documents/GitHub/rate-my-prof/csv/courses_final_woName.csv'
+INTO TABLE courses
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(course_name, course_code,campus_id,professor_id);
