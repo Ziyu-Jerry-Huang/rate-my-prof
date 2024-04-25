81fad2194d8ee2b96119c1c8291bb00638866287
YillonMask <yixinrui24@gmail.com>
2024-04-25 14:29:36 -0700

    combine insert and update function in one sql file

--- sql/Insert_Data_From_Excel.sql
+++ sql/Insert_Data_From_Excel.sql
@@ -1,4 +1,6 @@
 USE RateMyProfessor;
+
+-- Load date into professors table
 LOAD DATA LOCAL INFILE '/Users/xinruiyi/Documents/GitHub/rate-my-prof/csv/Professor.csv'
 INTO TABLE professors
 FIELDS TERMINATED BY ',' ENCLOSED BY '"'
@@ -6,6 +8,7 @@ LINES TERMINATED BY '\n'
 IGNORE 1 ROWS
 (name, campus_id);
 
+-- Load data into campus table
 LOAD DATA LOCAL INFILE '/Users/xinruiyi/Documents/GitHub/rate-my-prof/csv/Comments_final_matched.csv'
 INTO TABLE reviews
 FIELDS TERMINATED BY ','
@@ -13,9 +16,18 @@ LINES TERMINATED BY '\n'
 IGNORE 1 ROWS
 (course_id,professor_id,rating,comment,date_posted);
 
+-- Load data into campus table
 LOAD DATA LOCAL INFILE '/Users/xinruiyi/Documents/GitHub/rate-my-prof/csv/courses_final_woName.csv'
 INTO TABLE courses
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS
 (course_name, course_code,campus_id,professor_id);
+
+-- Update the rating of courses and professors
+UPDATE courses
+SET rating = (SELECT AVG(rating) FROM reviews WHERE course_id = courses.course_id);
+UPDATE professors
+SET rating = (SELECT AVG(rating)
+FROM reviews
+WHERE reviews.professor_id = professors.professor_id);
